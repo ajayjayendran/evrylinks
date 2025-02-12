b7e02d5953ac612f985abf02d7574ad47ed26668
Ajayjayendran Arulraj <ajayjayendran.ar@gmail.com>
2025-02-12 16:20:25 +0530

    update url-metadata

--- src/handlers/metadataHandler.ts
+++ src/handlers/metadataHandler.ts
@@ -2,6 +2,7 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import { metadataRequestSchema } from "../schemas/metadataSchema";
 import { fetchMetadata } from "../services/metadataService";
 import { successResponse, handleError } from "../utils/response";
+import urlMetadata from "url-metadata";
 
 export const metadataHandler = async (
   req: FastifyRequest,
@@ -9,9 +10,12 @@ export const metadataHandler = async (
 ) => {
   try {
     const payload = metadataRequestSchema.parse(req.body);
+    const data = await urlMetadata(payload.url);
+    console.log(data);
     const metadata = await fetchMetadata(payload);
-    reply.send(successResponse(metadata));
+    reply.send(successResponse(data));
   } catch (error) {
+    console.log(error);
     reply.status(500).send(handleError(error));
   }
 };
--- src/routes/metadata.ts
+++ src/routes/metadata.ts
@@ -2,5 +2,5 @@ import type { FastifyInstance } from "fastify";
 import { metadataHandler } from "../handlers/metadataHandler";
 
 export default async function metadataRoutes(fastify: FastifyInstance) {
-  fastify.post("/metadata", { handler: metadataHandler });
+  fastify.post("/meta", { handler: metadataHandler });
 }
